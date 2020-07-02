798004e47a4ba51576474f8e94cbf794958f4a3c
mgunnels-usgs <64610806+mgunnels-usgs@users.noreply.github.com>
2020-07-02 13:13:56 -0600

    Update compare_syns.py

--- compare_syns.py
+++ compare_syns.py
@@ -9,6 +9,11 @@ from scipy import signal
 import argparse
 from obspy import read, Stream, read_events
 import os
+import numpy as np
+from obspy.signal.cross_correlation import correlate, xcorr_max, xcorr
+from obspy.geodetics.base import gps2dist_azimuth
+
+
 
 # Making the figures look nice
 mpl.rc('font', family='serif')
@@ -24,6 +29,7 @@ def setup_data(net, sta, stime, etime, client):
         print('Metadata problem for: ' + sta)
         return [], False
     try:
+        # Wave form data
         st_data = client.get_waveforms(network=net, station=sta, starttime=stime,
                                    endtime = etime, channel = "LH*", location="*")
     except:
@@ -42,13 +48,14 @@ def main():
     client = Client(parser_val.client)
     debug = parser_val.debug
     net = parser_val.network
-
+    res_dir=parser_val.res_dir
     # Made this a bit smaller
     if not os.path.exists(parser_val.res_dir):
         os.mkdir(parser_val.res_dir)
 
-    userminfre = 1.0/float(parser_val.filter[1])
-    usermaxfre = 1.0/float(parser_val.filter[0])
+    # Get filter corners
+    userminfre = 1.0/float(parser_val.filter[0])
+    usermaxfre = 1.0/float(parser_val.filter[1])
     filtercornerpoles = int(parser_val.filter[2])
 
     if parser_val.sta:
@@ -60,49 +67,97 @@ def main():
     else:
         files = glob.glob(parser_val.syn + '/' + net + '*MXZ*')
 
-
+    # Synthetic Data
     syn = parser_val.syn
+    # Load in CMT Solution
+    cat = obspy.read_events(parser_val.syn + '/CMTSOLUTION')
 
-    eve = obspy.read_events(parser_val.syn + '/CMTSOLUTION')[0]
+    # Get event information
+    cmtlat = cat[0].origins[0].latitude
+    cmtlon = cat[0].origins[0].longitude
 
-    stf = eve.focal_mechanisms[0].moment_tensor.source_time_function
+    # Get source time function
+    stf = cat[0].focal_mechanisms[0].moment_tensor.source_time_function
+    # Apply a window
     win = signal.hann(int(2*stf['duration']))
 
+    # Load in files
     for cur_file in files:
         cur_file = cur_file.replace('MXZ', 'MX*')
         try:
             st = read(cur_file)
         except:
+            print('Directory Empty: No files to read in')
             continue
+    # Loop over waveform data
         for tr in st:
             tr.data = resample(tr.data, int(tr.stats.endtime - tr.stats.starttime))
             tr.stats.sampling_rate = 1.
             tr.data = signal.convolve(tr.data,win, mode='same')/sum(win)
 
-        print(st)
+
         st_data, coors = setup_data(net, st[-1].stats.station, st[-1].stats.starttime,
                                     st[-1].stats.endtime, client)
-
-        st += st_data
+        try:
+            st += st_data
+        except:
+            print('No Data for: ' + st[-1].stats.station)
+            continue
         st.filter('bandpass', freqmin=usermaxfre, freqmax=userminfre)
         st.taper(0.05)
 
+
+        # Open a file to write the correlation statistics
+        evename=(str(cat.resource_id).split('/')[-2])
+        statfile = open(os.getcwd() + '/' + res_dir + '/Results' + evename + net + '.csv' ,'w')
+        statfile.write('net, sta, loc, chan, scalefac, lag, corr, time\n')
+
+
+        # Plot data, calculate correlation statistics, and save plot to directory
         fig = plt.figure(1, figsize=(12, 12))
         for idx, chan in enumerate(['Z', 'N', 'E']):
+            if chan == 'Z':
+                plt.subplot(3,1,1)
+                title = st[0].stats.network + ' ' + \
+                        st[0].stats.station + ' '
+                starttime = st[0].stats.starttime
+                stime = str(starttime.year) + ' ' + str(starttime.julday) + \
+                        ' ' + str(starttime.hour) + ':' + \
+                        str(starttime.minute) + ':' + \
+                        str("{0:.2f}".format(starttime.second))
+                title += stime + ' '
+                lat= coors['latitude']
+                lon = coors['longitude']
+                dist = gps2dist_azimuth(float(cmtlat), float(cmtlon), lat, lon)
+                bazi = "{0:.1f}".format(dist[2])
+                dist = "{0:.1f}".format(0.0089932*dist[0]/1000.)
+                title += 'Dist:' + str(dist)
+                title += ' BAzi:' + str(bazi) + ' '
+                title += str("{0:.0f}".format(1/userminfre)) + '-' + \
+                         str("{0:.0f}".format(1/usermaxfre)) + ' s per.'
+                plt.title(title, fontsize=24)
+
+            #Get Correlation Statistics
+            try:
+                writestats(statfile, st, chan)
+            except:
+                print('Problem with: ' + sta)
+
             plt.subplot(3, 1, idx + 1)
             st_t = st.select(channel="*" + chan)
             for tr in st_t:
                 plt.plot(tr.times(), tr.data * 1000., label=tr.id)
                 plt.xlim((min(tr.times()), max(tr.times())))
+            # Only label subplot 2 y-axis
             if idx == 1:
                 filename = get_file_name(parser_val.res_dir, st_data)
-                # These units are wrong
-                plt.ylabel('Amplitude (m)')
-            plt.legend(loc=1)
+                plt.ylabel('Displacement (mm)')
+            plt.legend(prop={'size': 8}, loc=2)
         plt.xlabel('Time (s)')
         plt.savefig(filename + '.PNG', format='PNG', dpi=200)
         plt.clf()
         plt.close()
+
 
 def getargs():
     " Gets user arguments"
@@ -133,10 +188,10 @@ def getargs():
                         default=False, help="Run in debug mode")
 
     parser.add_argument('-filter', action="store", nargs=3, dest="filter", required=False,
-                        default=[300, 50, 4],
+                        default=[50, 150, 4],
                         help="Filter parameters using minimum period maximum period "
-                             "and number of corners Example: 50 300 4, "
-                             "default is 300, 50, 4")
+                             "and number of corners Example: 50 150 4, "
+                             "default is 50,150, 4")
 
     parser_val = parser.parse_args()
 
@@ -154,6 +209,36 @@ def get_file_name(res_dir,st_data):
         min=str(st_data[0].stats.starttime.minute)
     )
     return filename
+
+
+
+def writestats(statfile, st, chan):
+    """
+    calculate the correlation coefficient and lag time for the synthetic
+    when compared to the observed data and write to a file.
+    """
+    try:
+
+        syncomp = "MX" + chan
+        datacomp = "LH" + chan
+        syn = st.select(channel = syncomp)
+        for tr in st.select(channel = datacomp):
+            resi = "{0:.2f}".format(np.sum(tr.data*syn[0].data[:-1])/np.sum(np.square(syn[0].data[:-1])))
+            cc=correlate(tr,syn[0],500)
+            lag, corr = xcorr_max(cc)
+            corr = "{0:.2f}".format(corr)
+            #
+            statfile.write(tr.stats.network + "," + tr.stats.station)
+            statfile.write("," + tr.stats.location + "," + tr.stats.channel + "," +  str(resi))
+            statfile.write("," + str(lag) + "," + str(corr) + ", ")
+            statfile.write(str(tr.stats.starttime.month) + "/" + str(tr.stats.starttime.day) + \
+                                "/" + str(tr.stats.starttime.year) + " " + str(tr.stats.starttime.hour) + ":" + \
+                                str(tr.stats.starttime.minute) + ":" + str(tr.stats.starttime.second) + "\n")
+    except:
+        if debug:
+            print('No residual for' + tr.stats.station + ' ' + 'LH' + comp)
+    return
+
 
 
 if __name__ == "__main__":
